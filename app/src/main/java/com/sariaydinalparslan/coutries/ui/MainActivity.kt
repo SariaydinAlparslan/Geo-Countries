8c7caa292f3fead669d071b79ecc6c42fa59197d
Alparslan Sariaydin <sariaydinalparslan@gmail.com>
2022-09-10 00:07:14 +0300

    Login Register tamam

--- app/src/main/java/com/sariaydinalparslan/coutries/ui/MainActivity.kt
+++ app/src/main/java/com/sariaydinalparslan/coutries/ui/MainActivity.kt
@@ -4,13 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
 import com.sariaydinalparslan.coutries.R
 import kotlinx.android.synthetic.main.activity_modes.*
+private lateinit var databaseReference: DatabaseReference
+private  var firebaseUser: FirebaseUser? = null
+private lateinit var auth : FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+
+
     }
     fun findmatch(view: View) {
         val find = Intent(this@MainActivity, ModesActivity::class.java)
---
+++ app/src/main/java/com/sariaydinalparslan/coutries/ui/RegisterActivity.kt
@@ -0,0 +1,102 @@
+package com.sariaydinalparslan.coutries.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.sariaydinalparslan.coutries.R
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+    private  var firebaseUser: FirebaseUser? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        auth = FirebaseAuth.getInstance()
+
+       firebaseUser = auth!!.currentUser!!
+    if (firebaseUser != null) { val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+         startActivity(intent)
+         finish()
+             }
+    }
+
+    fun signup(view:View){
+        val userName = etName.text.toString()
+        val email = etEmail.text.toString()
+        val password = etPassword.text.toString()
+        val confirmPassword = etConfirmPassword.text.toString()
+        if (userName.isEmpty()) {
+            Toast.makeText(applicationContext, "username is required", Toast.LENGTH_SHORT)
+                .show()
+        }
+        if (email.isEmpty()) {
+            Toast.makeText(applicationContext, "email is required", Toast.LENGTH_SHORT).show()
+        }
+        if (password.isEmpty()) {
+            Toast.makeText(applicationContext, "password is required", Toast.LENGTH_SHORT)
+                .show()
+        }
+        if (confirmPassword.isEmpty()) {
+            Toast.makeText(
+                applicationContext,
+                "confirm password is required",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(applicationContext, "password not match", Toast.LENGTH_SHORT).show()
+        }
+        registerUser(userName,email,password)
+    }
+
+    fun login(view:View){
+        val intent = Intent(this@RegisterActivity,LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    private fun registerUser(userName: String, email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    val user: FirebaseUser? = auth.currentUser
+                    val userId: String = user!!.uid
+
+                    databaseReference =
+                        FirebaseDatabase.getInstance().getReference("Users").child(userId)
+
+                    val hashMap: HashMap<String, String> = HashMap()
+                    hashMap.put("userId", userId)
+                    hashMap.put("userName", userName)
+                    hashMap.put("profileImage", "")
+
+                    databaseReference.setValue(hashMap).addOnCompleteListener(this) {
+                        if (it.isSuccessful) {
+                            //open home activity
+                            etName.setText("")
+                            etEmail.setText("")
+                            etPassword.setText("")
+                            etConfirmPassword.setText("")
+                            val intent = Intent(
+                                this@RegisterActivity,
+                                MainActivity::class.java
+                            )
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+                }
+            }
+    }
+}
