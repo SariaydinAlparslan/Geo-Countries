c06b68a6a0e48afc7c055035f34623b53b386f4e
Alparslan Sariaydin <sariaydinalparslan@gmail.com>
2022-09-14 16:02:21 +0300

    2 roomdan gelen kişi singletondan seçilen ülke oyun activity de gözükmüyor bir yol firebaseden geçiyor bir yol users dataya ap ülkeyi kaydettikten sonra oradan çekmek.

---
+++ app/src/main/java/com/sariaydinalparslan/coutries/ui/Singleton.kt
@@ -0,0 +1,7 @@
+package com.sariaydinalparslan.coutries.ui
+
+object mySingleton{
+    var chosenlandmark: String? = null
+    var chosenCountry: String? = null
+    var hostCountry: String? = null
+}
