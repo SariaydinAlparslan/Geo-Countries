1ba8265422d71ecc85c44dd6bb318ee529c5718b
Alparslan Sariaydin <sariaydinalparslan@gmail.com>
2022-11-17 03:29:10 +0300

    changed ui game activity and guess country,added settings options and fixed start admob

--- app/src/main/java/com/sariaydinalparslan/coutries/ui/ui/SettingsFragment.kt
+++ app/src/main/java/com/sariaydinalparslan/coutries/ui/ui/SettingsFragment.kt
@@ -1,8 +1,11 @@
 package com.sariaydinalparslan.coutries.ui.ui
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
+import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -38,6 +41,28 @@ class SettingsFragment : Fragment() {
             binding.share.visibility = View.GONE
             binding.rate.visibility=View.GONE
           includeavatar.visibility = View.VISIBLE
+        }
+        share.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.setType("text/plain")
+            val string3 = "https://play.google.com/store/apps/details?id=com.sariaydinalparslan.coutries&pli=1"
+            shareIntent.putExtra(Intent.EXTRA_TEXT,string3)
+            startActivity(Intent.createChooser(shareIntent,"Deneme"))
+
+        }
+        rate.setOnClickListener {
+            val uri : Uri = Uri.parse("market://details?id=com.sariaydinalparslan.coutries&pli=1")
+            val gotoMarket = Intent(Intent.ACTION_VIEW,uri)
+
+            gotoMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or
+            Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+            Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+            )
+            try {
+                startActivity(gotoMarket)
+            }catch (e : ActivityNotFoundException){
+                startActivity(Intent(Intent.ACTION_VIEW,Uri.parse("https://play.google.com/store/apps/details?id=com.sariaydinalparslan.coutries&pli=1")))
+            }
         }
     }
     override fun onDestroyView() {
